ad4b17aa3ba761717993393dc02dae889b935ca8
filipriec <filippriec@gmail.com>
2023-11-14 16:35:00 +0100

    working logout, lang change menu in progress

--- add_product_main/static/js/change_lang.js
+++ add_product_main/static/js/change_lang.js
@@ -1,15 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var languageSelector = document.querySelector('.sidebar-menu-item-lang');
-  console.log('Language selector:', languageSelector); // Should not be null
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Access the language selector element
+//   var languageSelector = document.getElementById('languageSelect');
 
-  if (languageSelector) {
-    languageSelector.addEventListener('change', function() {
-      console.log('Language changed to:', this.value); // Check if this logs
-      var form = document.getElementById('languageForm');
-      console.log('Form:', form); // Should not be null
-      if (form) {
-        form.submit();
-      }
-    });
-  }
-});
+//   // Add an event listener for the 'change' event
+//   languageSelector.addEventListener('change', function() {
+//     // Access the form and submit it
+//     var languageForm = document.getElementById('languageForm');
+//     languageForm.submit();
+//   });
+// });
