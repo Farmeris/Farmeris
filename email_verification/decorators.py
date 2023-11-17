1feaab849ad6a9c8bb587cfb2a45548a33ce0331
filipriec <filippriec@gmail.com>
2023-11-18 00:17:51 +0100

    mail verification fixes

--- email_verification/decorators.py
+++ email_verification/decorators.py
@@ -3,19 +3,15 @@ from django.shortcuts import redirect
 from django.contrib import messages
 from functools import wraps
 from allauth.account.models import EmailAddress
+from django.contrib.auth.decorators import login_required
 
 def email_verification_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        # Get the user's primary email address or None if it doesn't exist
-        user = request.user
-        email_address = EmailAddress.objects.filter(user=user, primary=True).first()
-
-        # If the user has no primary email or it's not verified, allow access
-        if email_address is None or not email_address.verified:
+        # Check if the email_verified session variable is set
+        if request.session.get('email_verified'):
             return view_func(request, *args, **kwargs)
-
-        # Otherwise, the primary email is verified; show an error and redirect
-        messages.error(request, "Your primary email is already verified.")
-        return redirect('send_verification_email')
+        else:
+            messages.error(request, "You need to verify your email address to access this page.")
+            return redirect('send_verification_email')
     return _wrapped_view
